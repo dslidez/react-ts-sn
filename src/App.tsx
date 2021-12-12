60ec1a5ea8bc1faa28681ba26b8b770c88635fd9
Daniil <danya.zinovenko@mail.ru>
2021-12-12 11:27:03 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5VOIJTKhQNgBLepC2ZrFqrEJ4s8fKh0FySbaKu-kQk9IHaRPOo8h6nQ6oglSQX4GxciM&usqp=CAU" />
+      </header>
+      <nav className="nav">
+        <div>
+          {" "}
+          <a> Profile </a>{" "}
+        </div>
+        <div>
+          <a> Messages </a>
+        </div>
+        <div>
+          <a> News </a>
+        </div>
+        <div>
+          <a> Music </a>
+        </div>
+        <div>
+          <a> Settings </a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoxsa6Bi3ZEuSNlEpOJv9nu_HRNqCKMUAoGxasmY1kuvbmIgKy6csAGQ2Hfolit1f4LQs&usqp=CAU" />
+        </div>
+        <div>ava+description</div>
+        <div>
+          My posts
+          <div>New post</div>
+        </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
     </div>
   );
 };
