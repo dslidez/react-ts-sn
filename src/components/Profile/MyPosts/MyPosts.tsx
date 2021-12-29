132a6a699c269d4da2f486b9051807ac88a02548
Daniil <danya.zinovenko@mail.ru>
2021-12-29 21:15:29 +0300

    click

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -7,13 +7,19 @@ const MyPosts = (props: any) => {
 
   let postsElements = props.state.profilePage.posts.map( (p: any) =>  <Post message={p.message} likesCount={p.likesCount} /> )
 
+  let newPostElement = React.createRef()
+
+  let addPost = () => {
+    let text = newPostElement.current.value
+  }
+
   return (
     <div className={c.content}>
       <h3>
         My posts
         <h3>
-          <textarea></textarea>
-          <button>ADD POST</button>
+          <textarea ref={newPostElement}></textarea>
+          <button onClick={ addPost }>ADD POST</button>
         </h3>
       </h3>
       {postsElements}
