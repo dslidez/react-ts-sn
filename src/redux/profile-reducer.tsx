7b7f8fb0762b4638a6d5529527cff9de2eb4f214
Daniil <danya.zinovenko@mail.ru>
2022-01-16 23:35:44 +0300

    profile reducer refactor

--- src/redux/profile-reducer.tsx
+++ src/redux/profile-reducer.tsx
@@ -15,23 +15,23 @@ let initialState = {
 
 export let profileReducer = (state: any = initialState, action: any) => {
   switch (action.type) {
-    case ADD_POST:{
+
+    
+    case ADD_POST:
       let newPost: PostType = {
         id: 5,
         message: state.newPostText,
         likesCount: 0,
       };
-      let stateCopy = {...state} 
-      stateCopy.posts = [...state.posts] 
-      stateCopy.posts.push(newPost);
-      stateCopy.newPostText = "";
-      return stateCopy;
-    }
-    case UPDATE_NEW_POST_TEXT: {
-      let stateCopy = {...state} 
-      stateCopy.newPostText = action.newText;
-      return stateCopy;
-  }
+      return {...state,
+                       posts: [...state.posts, newPost],
+                       newPostText: ""  } 
+      
+    
+    case UPDATE_NEW_POST_TEXT: 
+      return {...state, 
+                       newPostText: action.newText} 
+      
     default:
       return state;
   }
