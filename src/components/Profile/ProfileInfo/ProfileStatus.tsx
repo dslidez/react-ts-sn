a4e1599b0c91bcf6674e5f7d80ba17549557ad10
Daniil <danya.zinovenko@mail.ru>
2022-02-03 20:04:37 +0300

    profile status local

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -11,17 +11,23 @@ export class ProfileStatus extends React.Component<any> {
       })
   }
 
+  deactivateEditMode = () => {
+      this.setState({
+          editMode: false
+      })
+  }
+
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
           </div>
         )}
         {this.state.editMode && (
           <div>
-            <input value={this.props.status} />
+            <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
           </div>
         )}
       </div>
